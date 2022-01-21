7b6331fc5339a15c620c10e449d36ec98ac19177
Mateusz Kostka <mateuszkostka5@gmail.com>
2022-01-21 16:05:43 +0100

    Move functions array_creation and calculate_neighbor to class as class method

--- main.py
+++ main.py
@@ -1,45 +1,38 @@
 from pole import Pole
 from typing import List
 
-#elements 1 is normal place; element 0 is przeszkoda; element -1 is meta; element 2 is start
-tablica_elementow=[[1,1,1,2,1],[1,1,0,0,1],[1,1,1,1,1],[1,0,0,1,1],[1,1,-1,0,1]]
+# elements 1 is normal place; element 0 is przeszkoda; element -1 is meta; element 2 is start
+tablica_elementow = [[1, 1, 1, 2, 1], [1, 1, 0, 0, 1],
+                     [1, 1, 1, 1, 1], [1, 0, 0, 1, 1], [1, 1, -1, 0, 1]]
 
-global_object_table=[]
 
-def array_creation(table_of_elements:List[List[int]]):
-    for row_counter,row in enumerate(table_of_elements):
-        row_object_table=[]
-        for columne_counter,element_value in enumerate(row):            
-            row_object_table.append(Pole(row_counter,columne_counter,element_value))
-        global_object_table.append(row_object_table)
+global_object_table = Pole.array_creation(tablica_elementow)
 
-def calculate_neighbor(object_table:List[List[Pole]]):
-    #simple mathematic reference
-    #change hardcoded values
-    #later add recursive finding
+
+def calculate_neighbor(object_table: List[List[Pole]]):
+    # simple mathematic reference
+    # change hardcoded values
+    # later add recursive finding
     for row in object_table:
         for element in row:
-            neighbor_addreses_up_y=element.y_position+1
-            neighbor_addreses_right_x=element.x_position+1
-            neighbor_addreses_down_y=element.y_position-1
-            neighbor_addreses_left_x=element.x_position-1
-            
-            if neighbor_addreses_up_y>=0 and neighbor_addreses_up_y<=4:
-                global_object_table[element.x_position][element.y_position].up=global_object_table[element.y_position][neighbor_addreses_up_y]
-            
-            if neighbor_addreses_right_x>=0 and neighbor_addreses_right_x<=4:
-                global_object_table[element.x_position][element.y_position].right=global_object_table[neighbor_addreses_right_x][element.y_position]
-            
-            if neighbor_addreses_down_y>=0 and neighbor_addreses_down_y<=4:
-                global_object_table[element.x_position][element.y_position].down=global_object_table[element.y_position][neighbor_addreses_down_y]
-            
-            if neighbor_addreses_left_x>=0 and neighbor_addreses_left_x<=4:
-                global_object_table[element.x_position][element.y_position].left=global_object_table[neighbor_addreses_left_x][element.y_position]
-                
-            
+            neighbor_addreses_up_y = element.y_position+1
+            neighbor_addreses_right_x = element.x_position+1
+            neighbor_addreses_down_y = element.y_position-1
+            neighbor_addreses_left_x = element.x_position-1
+
+            if neighbor_addreses_up_y >= 0 and neighbor_addreses_up_y <= 4:
+                global_object_table[element.x_position][element.y_position].up = global_object_table[element.y_position][neighbor_addreses_up_y]
+
+            if neighbor_addreses_right_x >= 0 and neighbor_addreses_right_x <= 4:
+                global_object_table[element.x_position][element.y_position].right = global_object_table[neighbor_addreses_right_x][element.y_position]
+
+            if neighbor_addreses_down_y >= 0 and neighbor_addreses_down_y <= 4:
+                global_object_table[element.x_position][element.y_position].down = global_object_table[element.y_position][neighbor_addreses_down_y]
+
+            if neighbor_addreses_left_x >= 0 and neighbor_addreses_left_x <= 4:
+                global_object_table[element.x_position][element.y_position].left = global_object_table[neighbor_addreses_left_x][element.y_position]
 
 
-array_creation(table_of_elements=tablica_elementow)
 calculate_neighbor(global_object_table)
 
 
--- pole.py
+++ pole.py
@@ -1,17 +1,28 @@
+from typing import List
+
 
 class Pole:
-    #this filds are for reference to neighbors
-    up=None
-    right=None
-    down=None
-    left=None
-    def __init__(self,x:int,y:int,value:int):
-        self.x_position=x
-        self.y_position=y
-        self.value=value
+    # this filds are for reference to neighbors
+    up = None
+    right = None
+    down = None
+    left = None
+    global_object_table = []
 
-
+    def __init__(self, x: int, y: int, value: int):
+        self.x_position = x
+        self.y_position = y
+        self.value = value
 
     def __str__(self):
         return f"X:{self.x_position};Y:{self.y_position};Value:{self.value}"
-        
+
+    @classmethod
+    def array_creation(cls, table_of_elements: List[List[int]]):
+        for row_counter, row in enumerate(table_of_elements):
+            row_object_table = []
+            for columne_counter, element_value in enumerate(row):
+                row_object_table.append(
+                    Pole(row_counter, columne_counter, element_value))
+            cls.global_object_table.append(row_object_table)
+        return cls.global_object_table
