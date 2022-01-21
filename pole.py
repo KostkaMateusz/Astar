999ac3b93afcf2ac9a9b1b8247c78c1f3a13e01a
Mateusz Kostka <mateuszkostka5@gmail.com>
2022-01-21 15:49:14 +0100

    Add basic class

--- pole.py
+++ pole.py
@@ -1,9 +1,16 @@
 
 class Pole:
+    #this filds are for reference to neighbors
+    up=None
+    right=None
+    down=None
+    left=None
     def __init__(self,x:int,y:int,value:int):
         self.x_position=x
         self.y_position=y
         self.value=value
+
+
 
     def __str__(self):
         return f"X:{self.x_position};Y:{self.y_position};Value:{self.value}"
