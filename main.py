6c483d3d3822173f8108565b553b936353b99576
Mateusz Kostka <mateuszkostka5@gmail.com>
2022-01-21 16:50:26 +0100

    Add auto neighbour calc; fix position of elements

--- main.py
+++ main.py
@@ -9,32 +9,13 @@ tablica_elementow = [[1, 1, 1, 2, 1], [1, 1, 0, 0, 1],
 global_object_table = Pole.array_creation(tablica_elementow)
 
 
-def calculate_neighbor(object_table: List[List[Pole]]):
-    # simple mathematic reference
-    # change hardcoded values
-    # later add recursive finding
-    for row in object_table:
-        for element in row:
-            neighbor_addreses_up_y = element.y_position+1
-            neighbor_addreses_right_x = element.x_position+1
-            neighbor_addreses_down_y = element.y_position-1
-            neighbor_addreses_left_x = element.x_position-1
-
-            if neighbor_addreses_up_y >= 0 and neighbor_addreses_up_y <= 4:
-                global_object_table[element.x_position][element.y_position].up = global_object_table[element.y_position][neighbor_addreses_up_y]
-
-            if neighbor_addreses_right_x >= 0 and neighbor_addreses_right_x <= 4:
-                global_object_table[element.x_position][element.y_position].right = global_object_table[neighbor_addreses_right_x][element.y_position]
-
-            if neighbor_addreses_down_y >= 0 and neighbor_addreses_down_y <= 4:
-                global_object_table[element.x_position][element.y_position].down = global_object_table[element.y_position][neighbor_addreses_down_y]
-
-            if neighbor_addreses_left_x >= 0 and neighbor_addreses_left_x <= 4:
-                global_object_table[element.x_position][element.y_position].left = global_object_table[neighbor_addreses_left_x][element.y_position]
+for row in global_object_table:
+    for element in row:
+        print(element)
+        print("UP:"+str(element.up))
+        print("LEFT:"+str(element.left))
+        print("DOWN:"+str(element.down))
+        print("RIGHT:"+str(element.right))
+        print("________________")
 
 
-calculate_neighbor(global_object_table)
-
-
-print(global_object_table[4][3])
-print(global_object_table[4][3].right)
--- pole.py
+++ pole.py
@@ -18,11 +18,40 @@ class Pole:
         return f"X:{self.x_position};Y:{self.y_position};Value:{self.value}"
 
     @classmethod
-    def array_creation(cls, table_of_elements: List[List[int]]):
+    def array_creation(cls, table_of_elements: List[List[int]]) -> List[List]:
         for row_counter, row in enumerate(table_of_elements):
             row_object_table = []
             for columne_counter, element_value in enumerate(row):
-                row_object_table.append(
-                    Pole(row_counter, columne_counter, element_value))
+                row_object_table.append(Pole(columne_counter,row_counter, element_value))
             cls.global_object_table.append(row_object_table)
+        # after creation of a array of object calculate references to neighbors
+        Pole.calculate_neighbors()   
         return cls.global_object_table
+
+    @classmethod
+    def calculate_neighbors(cls):
+        # simple mathematic reference
+        # change hardcoded values
+        # later add recursive finding
+        for row in cls.global_object_table:
+            for element in row:
+                neighbor_addreses_up_y = element.y_position-1
+                neighbor_addreses_right_x = element.x_position+1
+                neighbor_addreses_down_y = element.y_position+1
+                neighbor_addreses_left_x = element.x_position-1
+
+                if neighbor_addreses_up_y >= 0 and neighbor_addreses_up_y <= 4:
+                    cls.global_object_table[element.y_position][element.x_position].up = cls.global_object_table[neighbor_addreses_up_y][
+                        element.x_position]
+
+                if neighbor_addreses_right_x >= 0 and neighbor_addreses_right_x <= 4:
+                    cls.global_object_table[element.y_position][element.x_position].right = cls.global_object_table[element.y_position][
+                        neighbor_addreses_right_x]
+
+                if neighbor_addreses_down_y >= 0 and neighbor_addreses_down_y <= 4:
+                    cls.global_object_table[element.y_position][element.x_position].down = cls.global_object_table[neighbor_addreses_down_y][
+                        element.x_position]
+
+                if neighbor_addreses_left_x >= 0 and neighbor_addreses_left_x <= 4:
+                    cls.global_object_table[element.y_position][element.x_position].left = cls.global_object_table[element.y_position][
+                        neighbor_addreses_left_x]
