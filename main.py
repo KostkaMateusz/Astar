74b3a3c2df60c5069586fd215f0dfaaca5dedba5
Mateusz Kostka <mateuszkostka5@gmail.com>
2022-01-21 17:10:12 +0100

    Fix bugs, add method to print all elements

--- main.py
+++ main.py
@@ -9,13 +9,4 @@ tablica_elementow = [[1, 1, 1, 2, 1], [1, 1, 0, 0, 1],
 global_object_table = Pole.array_creation(tablica_elementow)
 
 
-for row in global_object_table:
-    for element in row:
-        print(element)
-        print("UP:"+str(element.up))
-        print("LEFT:"+str(element.left))
-        print("DOWN:"+str(element.down))
-        print("RIGHT:"+str(element.right))
-        print("________________")
-
 
--- pole.py
+++ pole.py
@@ -8,6 +8,9 @@ class Pole:
     down = None
     left = None
     global_object_table = []
+    h=0
+    g=0
+    f=h+g
 
     def __init__(self, x: int, y: int, value: int):
         self.x_position = x
@@ -16,6 +19,17 @@ class Pole:
 
     def __str__(self):
         return f"X:{self.x_position};Y:{self.y_position};Value:{self.value}"
+
+    @classmethod
+    def print_all_items(cls):        
+        for row in cls.global_object_table:
+            for element in row:
+                print(element)
+                print("UP:"+str(element.up))
+                print("LEFT:"+str(element.left))
+                print("DOWN:"+str(element.down))
+                print("RIGHT:"+str(element.right))
+                print("________________")
 
     @classmethod
     def array_creation(cls, table_of_elements: List[List[int]]) -> List[List]:
@@ -30,8 +44,7 @@ class Pole:
 
     @classmethod
     def calculate_neighbors(cls):
-        # simple mathematic reference
-        # change hardcoded values
+        # simple mathematic reference        
         # later add recursive finding
         for row in cls.global_object_table:
             for element in row:
@@ -40,18 +53,18 @@ class Pole:
                 neighbor_addreses_down_y = element.y_position+1
                 neighbor_addreses_left_x = element.x_position-1
 
-                if neighbor_addreses_up_y >= 0 and neighbor_addreses_up_y <= 4:
+                if neighbor_addreses_up_y >= 0 and neighbor_addreses_up_y <= len(cls.global_object_table)-1:
                     cls.global_object_table[element.y_position][element.x_position].up = cls.global_object_table[neighbor_addreses_up_y][
                         element.x_position]
 
-                if neighbor_addreses_right_x >= 0 and neighbor_addreses_right_x <= 4:
+                if neighbor_addreses_right_x >= 0 and neighbor_addreses_right_x <= len(row)-1:
                     cls.global_object_table[element.y_position][element.x_position].right = cls.global_object_table[element.y_position][
                         neighbor_addreses_right_x]
 
-                if neighbor_addreses_down_y >= 0 and neighbor_addreses_down_y <= 4:
+                if neighbor_addreses_down_y >= 0 and neighbor_addreses_down_y <= len(cls.global_object_table)-1:
                     cls.global_object_table[element.y_position][element.x_position].down = cls.global_object_table[neighbor_addreses_down_y][
                         element.x_position]
 
-                if neighbor_addreses_left_x >= 0 and neighbor_addreses_left_x <= 4:
+                if neighbor_addreses_left_x >= 0 and neighbor_addreses_left_x <= len(row)-1:
                     cls.global_object_table[element.y_position][element.x_position].left = cls.global_object_table[element.y_position][
                         neighbor_addreses_left_x]
