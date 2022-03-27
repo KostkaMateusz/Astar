cf29dfbddf08a16587e47f83c55ed6569f357115
Alakeks <mateuszkostka5@gmail.com>
2022-03-28 00:15:07 +0200

    Add heat map

---
+++ astar.py
@@ -0,0 +1,95 @@
+from pole import Pole
+from typing import List
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# elements 1 is normal place; element 0 is przeszkoda; element -1 is meta; element 2 is start
+tablica_elementow = [
+    [1, 1, 1, 2, 1],
+    [1, 1, 0, 0, 1],
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 1, 1],
+    [1, 1, -1, 1, 0],
+]
+
+global_object_table = Pole.array_creation(tablica_elementow)
+
+# a star engine
+
+
+def distance_calculator(place1: Pole, place2: Pole) -> int:
+    distance_x = abs(place1.x_position - place2.x_position)
+    distance_y = abs(place1.y_position - place2.y_position)
+    return distance_x + distance_y
+
+
+def a_star_engine(global_object_table: List[List[Pole]]):
+
+    open_list = []
+    closed_list = []
+
+    open_list.append(global_object_table[0][3])
+    finded = False
+    while len(open_list) != 0 and finded != True:
+        minimum = min([element.value_of_f for element in open_list])
+        index_with_min = -1
+        for index, element in enumerate(open_list):
+            if element.value_of_f == minimum:
+                index_with_min = index
+                break
+
+        q = open_list.pop(index_with_min)
+        for neigh in q.list_of_neighbors:
+            if neigh is not None and neigh.value != 0:
+                if neigh.value == -1:
+                    print("Znalazłem")
+                    finded = True
+                    break
+                neigh.g = q.g + distance_calculator(neigh, q)
+                neigh.h = distance_calculator(neigh, global_object_table[4][3])
+                if neigh in open_list:
+                    continue
+                if neigh in closed_list:
+                    continue
+                print(f"X:{neigh.x_position} Y:{neigh.y_position}")
+                open_list.append(neigh)
+
+        closed_list.append(q)
+
+
+a_star_engine(global_object_table)
+Pole.print_table()
+
+array_of_values = []
+
+for row in global_object_table:
+    table_row_values = []
+
+    for element in row:
+
+        table_row_values.append(element.value)
+
+    array_of_values.append(table_row_values)
+
+
+values = np.array(array_of_values)
+
+fig, ax = plt.subplots()
+im = ax.imshow(values)
+
+# # Show all ticks and label them with the respective list entries
+ax.set_xticks(np.arange(5), labels=[0, 1, 2, 3, 4])
+ax.set_yticks(np.arange(5), labels=[0, 1, 2, 3, 4])
+
+# # # Rotate the tick labels and set their alignment.
+plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+
+# Loop over data dimensions and create text annotations.
+for i in range(len(global_object_table)):
+    for j in range(len(global_object_table[1])):
+        text = ax.text(j, i, values[i, j], ha="center", va="center", color="w")
+
+ax.set_title("Astar")
+fig.tight_layout()
+plt.show()
--- pole.py
+++ pole.py
@@ -1,6 +1,3 @@
-from typing import List
-
-
 class Pole:
     # this filds are for reference to neighbors
     up = None
@@ -8,10 +5,9 @@ class Pole:
     down = None
     left = None
     global_object_table = []
-    h=0
-    g=0
-    
-  
+    h = 0
+    g = 0
+
     def __init__(self, x: int, y: int, value: int):
         self.x_position = x
         self.y_position = y
@@ -19,72 +15,97 @@ class Pole:
 
     def __str__(self):
         return f"X:{self.x_position};Y:{self.y_position};Value:{self.value}"
-    
 
     @property
     def list_of_neighbors(self):
-        return [self.up,self.right,self.down,self.left]
-    
+        return [self.up, self.right, self.down, self.left]
+
     @property
     def value_of_f(self):
-        return self.h+self.g
-    
+        return self.h + self.g
+
     @classmethod
     def print_table(cls):
         for row in cls.global_object_table:
-            table_row=""
+            table_row = ""
             for element in row:
-                table_row+=f"X:{element.x_position} Y:{element.y_position} F:{element.value_of_f} G:{element.g} H:{element.h} |"
+                table_row += f"X:{element.x_position} Y:{element.y_position} F:{element.value_of_f} G:{element.g} H:{element.h} |"
             print(table_row)
-    
+
     @classmethod
-    def print_all_items(cls):        
+    def print_all_items(cls):
         for row in cls.global_object_table:
             for element in row:
                 print(element)
-                print("UP:"+str(element.up))
-                print("LEFT:"+str(element.left))
-                print("DOWN:"+str(element.down))
-                print("RIGHT:"+str(element.right))
-                print("H:"+str(element.h))
-                print("G:"+str(element.g))
-                print("F:"+str(element.value_of_f))
+                print("UP:" + str(element.up))
+                print("LEFT:" + str(element.left))
+                print("DOWN:" + str(element.down))
+                print("RIGHT:" + str(element.right))
+                print("H:" + str(element.h))
+                print("G:" + str(element.g))
+                print("F:" + str(element.value_of_f))
                 print("________________")
 
     @classmethod
-    def array_creation(cls, table_of_elements: List[List[int]]) -> List[List]:
+    def array_creation(cls, table_of_elements: list[list[int]]) -> list[list]:
         for row_counter, row in enumerate(table_of_elements):
             row_object_table = []
             for columne_counter, element_value in enumerate(row):
-                row_object_table.append(Pole(columne_counter,row_counter, element_value))
+                row_object_table.append(
+                    Pole(columne_counter, row_counter, element_value)
+                )
             cls.global_object_table.append(row_object_table)
         # after creation of a array of object calculate references to neighbors
-        Pole.calculate_neighbors()   
+        Pole.calculate_neighbors()
         return cls.global_object_table
 
     @classmethod
     def calculate_neighbors(cls):
-        # simple mathematic reference        
+        # simple mathematic reference
         # later add recursive finding
         for row in cls.global_object_table:
             for element in row:
-                neighbor_addreses_up_y = element.y_position-1
-                neighbor_addreses_right_x = element.x_position+1
-                neighbor_addreses_down_y = element.y_position+1
-                neighbor_addreses_left_x = element.x_position-1
+                neighbor_addreses_up_y = element.y_position - 1
+                neighbor_addreses_right_x = element.x_position + 1
+                neighbor_addreses_down_y = element.y_position + 1
+                neighbor_addreses_left_x = element.x_position - 1
 
-                if neighbor_addreses_up_y >= 0 and neighbor_addreses_up_y <= len(cls.global_object_table)-1:
-                    cls.global_object_table[element.y_position][element.x_position].up = cls.global_object_table[neighbor_addreses_up_y][
-                        element.x_position]
+                if (
+                    neighbor_addreses_up_y >= 0
+                    and neighbor_addreses_up_y <= len(cls.global_object_table) - 1
+                ):
+                    cls.global_object_table[element.y_position][
+                        element.x_position
+                    ].up = cls.global_object_table[neighbor_addreses_up_y][
+                        element.x_position
+                    ]
 
-                if neighbor_addreses_right_x >= 0 and neighbor_addreses_right_x <= len(row)-1:
-                    cls.global_object_table[element.y_position][element.x_position].right = cls.global_object_table[element.y_position][
-                        neighbor_addreses_right_x]
+                if (
+                    neighbor_addreses_right_x >= 0
+                    and neighbor_addreses_right_x <= len(row) - 1
+                ):
+                    cls.global_object_table[element.y_position][
+                        element.x_position
+                    ].right = cls.global_object_table[element.y_position][
+                        neighbor_addreses_right_x
+                    ]
 
-                if neighbor_addreses_down_y >= 0 and neighbor_addreses_down_y <= len(cls.global_object_table)-1:
-                    cls.global_object_table[element.y_position][element.x_position].down = cls.global_object_table[neighbor_addreses_down_y][
-                        element.x_position]
+                if (
+                    neighbor_addreses_down_y >= 0
+                    and neighbor_addreses_down_y <= len(cls.global_object_table) - 1
+                ):
+                    cls.global_object_table[element.y_position][
+                        element.x_position
+                    ].down = cls.global_object_table[neighbor_addreses_down_y][
+                        element.x_position
+                    ]
 
-                if neighbor_addreses_left_x >= 0 and neighbor_addreses_left_x <= len(row)-1:
-                    cls.global_object_table[element.y_position][element.x_position].left = cls.global_object_table[element.y_position][
-                        neighbor_addreses_left_x]
+                if (
+                    neighbor_addreses_left_x >= 0
+                    and neighbor_addreses_left_x <= len(row) - 1
+                ):
+                    cls.global_object_table[element.y_position][
+                        element.x_position
+                    ].left = cls.global_object_table[element.y_position][
+                        neighbor_addreses_left_x
+                    ]
