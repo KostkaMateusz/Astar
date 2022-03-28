f10c81f74d0e6d2249a4fe50f4f2846894951325
Alakeks <mateuszkostka5@gmail.com>
2022-03-28 16:31:27 +0200

    Plot

--- astar.py
+++ astar.py
@@ -3,23 +3,32 @@ from plot import create_plot
 
 
 # elements 1 is normal place; element 0 is przeszkoda; element -1 is meta; element 2 is start
+
+start_x = 0
+start_y = 3
+end_x = 4
+end_y = 2
+
+
 tablica_elementow = [
     [1, 1, 1, 2, 1],
     [1, 1, 0, 0, 1],
     [1, 1, 1, 1, 1],
-    [1, 0, 0, 1, 1],
+    [1, 1, 0, 1, 1],
     [1, 1, -1, 1, 0],
 ]
 
 global_object_table = Pole.array_creation(tablica_elementow)
 
 
-def a_star_engine(global_object_table: list[list[Pole]]):
+def a_star_engine(
+    global_object_table: list[list[Pole]], start_x, start_y, end_x, end_y
+):
 
     open_list = []
     closed_list = []
 
-    open_list.append(global_object_table[0][3])
+    open_list.append(global_object_table[start_x][start_y])
     finded = False
     while len(open_list) != 0 and finded != True:
         minimum = min([element.value_of_f for element in open_list])
@@ -37,7 +46,9 @@ def a_star_engine(global_object_table: list[list[Pole]]):
                     finded = True
                     break
                 neigh.g = q.g + Pole.distance_calculator(neigh, q)
-                neigh.h = Pole.distance_calculator(neigh, global_object_table[4][3])
+                neigh.h = Pole.distance_calculator(
+                    neigh, global_object_table[end_x][end_y]
+                )
                 if neigh in open_list:
                     continue
                 if neigh in closed_list:
@@ -50,5 +61,5 @@ def a_star_engine(global_object_table: list[list[Pole]]):
 
 if __name__ == "__main__":
 
-    a_star_engine(global_object_table)
+    a_star_engine(global_object_table, 0, 3, 4, 3)
     create_plot(global_object_table)
--- plot.py
+++ plot.py
@@ -11,12 +11,21 @@ def create_plot(object_table: list[list[Pole]]):
     im = ax.imshow(values)
 
     # # Show all ticks and label them with the respective list entries
-    ax.set_xticks(np.arange(5), labels=[0, 1, 2, 3, 4])
-    ax.set_yticks(np.arange(5), labels=[0, 1, 2, 3, 4])
+    ax.set_xticks(
+        np.arange(len(object_table[0])),
+        labels=[element for element in range(len(object_table[0]))],
+    )
+    ax.set_yticks(
+        np.arange(len(object_table)),
+        labels=[element for element in range(len(object_table))],
+    )
 
     # # # Rotate the tick labels and set their alignment.
     plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
-
+    plt.axvline(x=2, color="red")
+    plt.axhline(y=4, color="red")
+    rectangle = plt.Rectangle((0, 0), 1, 2, ec="red")
+    plt.gca().add_patch(rectangle)
     # Loop over data dimensions and create text annotations.
     for i in range(len(object_table)):
         for j in range(len(object_table[1])):
