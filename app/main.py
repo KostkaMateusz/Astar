b22541f4db5be1fc61500a411e5e1584d092b52b
Alakeks <mateuszkostka5@gmail.com>
2022-03-30 02:42:51 +0200

    Add html landing page

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 import io
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
@@ -17,15 +19,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.post("/")
-def root(aStarParams: AStarParams):
+
+@app.get("/")
+async def root():
+    return RedirectResponse("/static/index.html")
+
+
+@app.post("/engine")
+async def root(aStarParams: AStarParams):
 
     dict_of_parameters = dict([ele for ele in aStarParams])
 
     image = io.BytesIO()
     img = generate_image(**dict_of_parameters)
-    img.savefig(image, format="png", dpi=300, transparent=True)
+    img.savefig(image, format="png", dpi=600, transparent=True)
     image.seek(0)
 
     return StreamingResponse(image, media_type="image/png")
